5dc83fdbc03f9f73fd1760b0a14a681955a63a14
margrun <57048880+margrun@users.noreply.github.com>
2019-12-31 16:14:49 +0100

    Update vue.js

--- vue.js
+++ vue.js
@@ -7,7 +7,7 @@ function randomString() {
   //const first = randomDigit(1);
   //const rest = [...Array(length - 1)].map(num => randomDigit());
   //return [first, ...rest].join('').trim();
-  var things = ['banana peel', 'church', 'sleep', 'money', 'kite', 'sailboat', 'popsicle', 'brain', 'birthday', 'cakeskirtknee', 'saxophone', 'crater', 'cage'];
+  var things = ['banana peel', 'church', 'sleep', 'money', 'kite', 'sailboat', 'popsicle', 'brain', 'birthday', 'cake', 'skirt', 'knee', 'saxophone', 'crater', 'cage', 'sailboat', 'popsicle', 'brain', 'birthday cake', 'skirt', 'knee', 'pineapple', 'tusk', 'sprinkler', 'money', 'spool', 'lighthouse', 'doormat', 'face', 'flute', 'rug', 'snow', 'ball', 'purse', 'owl', 'gate', 'suitcase', 'stomach', 'doghouse', 'pajamas', 'bathroom', 'scale', 'peach', 'newspaper', 'wateringcan', 'hook', 'school', 'beaver', 'french fries', 'beehive', 'beach', 'artist', 'flagpole'];
   var thing = things[Math.floor(Math.random()*things.length)];
   return thing;
   
